e617a5a1ebd7aebdf94e8467a01be265eb2de2f2
Manikanta7520 <manikanta7520@gmail.com>
2025-11-14 22:35:38 +0530

    Add files via upload
    Developers Arena Intership program 
    Week 1 - Task Submission

---
+++ TASK-1.py
@@ -0,0 +1,25 @@
+# Personal Introduction Program
+
+
+
+# Asking for user details
+
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+hobby = input("Enter your favorite hobby: ")
+
+
+
+# Displaying the personalized welcome message
+
+print("\n--- Personal Introduction ---")
+
+print(f"Hello {name}! Welcome to the program.")
+
+print(f"You are {age} years old and you love {hobby}.")
+
+print("It's great to know more about you! ")
+ 
+print("Welcome to Developers Arena Internship Program")
